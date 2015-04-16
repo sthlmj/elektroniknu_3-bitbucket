dd7273eb6820b70ecc67bfe0d05f3206cd4feca6
jacobkristersson <jacob@kristersson.com>
2015-04-16 09:22:28 +0200

    ElektronikNU_ListView
    En enkelt exempel på elektronikNU.
    Använder ListView med en array av strings.

---
+++ app/src/main/java/com/wordpress/elektroniknu/elektroniknu/MainActivity.java
@@ -0,0 +1,65 @@
+package com.wordpress.elektroniknu.elektroniknu;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Data that I want too put in my ListView
+        // Array of Strings
+        String[] electronicSupplier = {"Siba", "Elgiganten", "MediamMarkt", "NetOnNet"};
+
+        // ListAdapter too be able to adapt our array in too
+        // something that our listview is able to work with
+
+
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,
+                electronicSupplier);
+
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        //theListView.setOnItemClickListener(new
+        //AdapterView,onItemClickListner());
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
