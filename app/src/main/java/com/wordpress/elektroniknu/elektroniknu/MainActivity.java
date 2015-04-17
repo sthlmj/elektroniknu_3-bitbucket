a4a9d8eaa6ec1164e7d19495c9ab564f151afb2c
vgrigorjeva <vgrigorjeva@gmail.com>
2015-04-17 10:36:48 +0200

    Med_button

--- app/src/main/java/com/wordpress/elektroniknu/elektroniknu/MainActivity.java
+++ app/src/main/java/com/wordpress/elektroniknu/elektroniknu/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.Gravity;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -71,22 +73,31 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //  return true;
+        //}
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Toast toast = Toast.makeText(getBaseContext(), "Om oss: \n The best team in the world!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP|Gravity.RIGHT, 5, 10);
+                toast.show();
+                return true;
 
+
+        }
         return super.onOptionsItemSelected(item);
     }
+
 }
