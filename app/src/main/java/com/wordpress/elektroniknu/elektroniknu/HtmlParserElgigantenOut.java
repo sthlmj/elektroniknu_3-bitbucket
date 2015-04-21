854e50a8f52c7a811d1095e02b90c68e8b60b5cf
Huan Xie <cathy1985271@gmail.com>
2015-04-21 13:06:36 +0200

    skapa filen htmlparserelgigantenout

---
+++ app/src/main/java/com/wordpress/elektroniknu/elektroniknu/HtmlParserElgigantenOut.java
@@ -0,0 +1,51 @@
+package com.wordpress.elektroniknu.elektroniknu;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HtmlParserElgigantenOut {
+
+    public static void main(String[] args) {
+        getCatergoryList();
+    }
+
+    public static String[] getCatergoryList() {
+        Document Doc = null;
+        try {
+            Doc = Jsoup.connect("http://www.elgiganten.se/cms/veckans-annons/veckans-erbjudanden").get(); // HTML file from their website
+        } catch (IOException ex) { // Catch exception
+            Logger.getLogger(HtmlParserElgigantenOut.class.getName()).log(Level.SEVERE, null, ex); // Print out at log
+        }
+        if (Doc != null)
+        {
+            org.jsoup.select.Elements links = Doc.select("div.article-text.M-1-1.S-1-1 h2 a");
+            String[] catergoryList = new String[links.size()];
+            int i = 0;
+            for (Element e : links)
+            {
+                catergoryList[i] = e.attr("abs:href");
+                i++;
+            }
+            for(int j = 0; j < links.size(); j++ )
+            {
+                System.out.println(catergoryList[j]);
+            }
+            return catergoryList;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+
+
