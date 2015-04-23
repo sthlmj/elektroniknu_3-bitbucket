1ed3c67d470e5b9a094b4307d74538a0c4d726c8
staylin <admin@staylin.com>
2015-04-23 12:17:41 +0200

    Commented Category

--- app/src/main/java/com/wordpress/elektroniknu/elektroniknu/Category.java
+++ app/src/main/java/com/wordpress/elektroniknu/elektroniknu/Category.java
@@ -3,31 +3,33 @@ package com.wordpress.elektroniknu.elektroniknu;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Created by Aylin on 2015-04-21.
- */
 public class Category {
+
+    //PROPERTIES OF A CATEGORY
     private String categoryName;
     private List<Product> productList;
 
-    public void setCategoryName(String categoryName){
-        this.categoryName = categoryName;
+    //CATEGORY CONSTRUCTOR - creates a product list for the category, need no input
+    public Category(){
+        this.productList = new LinkedList<Product>();
     }
 
-    public String getCategoryName() {
-        return categoryName;
-    }
-
+    //ADD A PRODUCT INTO PRODUCT LIST
     public void addProduct(Product product){
         productList.add(product);
         product.setCategoryName(categoryName);
     }
 
+    //SET NAME OF CATEGORY
+    public void setCategoryName(String categoryName){
+        this.categoryName = categoryName;
+    }
+    //GET NAME OF CATEGORY
+    public String getCategoryName() {
+        return categoryName;
+    }
+    //GET THE LIST OF ALL PRODUCTS
     public List<Product> getProductList() {
         return productList;
-    }
-
-    public Category(){
-        this.productList = new LinkedList<Product>();
     }
 }
