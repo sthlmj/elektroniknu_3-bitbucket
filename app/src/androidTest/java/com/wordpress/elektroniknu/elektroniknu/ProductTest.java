e8957443f009d6aa8f5e115232869c85dffa9bb4
chenzimin <chen.zimin@hotmail.com>
2015-04-28 11:06:04 +0200

    Uncompletted test

--- app/src/androidTest/java/com/wordpress/elektroniknu/elektroniknu/ProductTest.java
+++ app/src/androidTest/java/com/wordpress/elektroniknu/elektroniknu/ProductTest.java
@@ -1,8 +1,10 @@
 package com.wordpress.elektroniknu.elektroniknu;
 
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
+import android.test.TouchUtils;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.ListView;
@@ -10,24 +12,18 @@ import android.widget.ListView;
 /**
  * Created by chenz_000 on 2015-04-27.
  */
-public class ProductActivityTest extends ActivityUnitTestCase<ProductActivity> {
+public class ProductTest extends ActivityInstrumentationTestCase2<ProductActivity> {
 
     private ProductActivity activity;
-    private ListView listView;
-    private View child;
     private Category category;
     private Product product;
-    private Intent intent;
 
-    public ProductActivityTest() {
+    public ProductTest() {
         super(ProductActivity.class);
     }
 
     protected void setUp() throws Exception{
         super.setUp();
-
-        ContextThemeWrapper context = new ContextThemeWrapper(getInstrumentation().getTargetContext(), R.style.AppTheme);
-        setActivityContext(context);
 
         product = new Product();
         product.setStoreName("Elgiganten");
@@ -48,33 +44,21 @@ public class ProductActivityTest extends ActivityUnitTestCase<ProductActivity> {
         Intent intent = new Intent(getInstrumentation().getTargetContext(),
                 MainActivity.class);
         intent.putExtra("Category", (java.io.Serializable) category);
-        this.intent = intent;
+        setActivityIntent(intent);
+        activity = getActivity();
     }
 
     public void testActivityNotNull(){
-        startActivity(intent, null, null);
-        activity = getActivity();
         assertNotNull(activity);
     }
 
     public void testPerfomItemClick() {
-        activity = startActivity(intent, null, null);
-        getInstrumentation().callActivityOnStart(activity);
-        getInstrumentation().callActivityOnResume(activity);
-
         ListView listView = (ListView) activity.findViewById(R.id.ProductListView);
         assertNotNull(listView);
-
-        Product[] products = new Product[1];
-        products[0] = product;
-        productsAdapter myAdapter = new productsAdapter(getInstrumentation().getContext(), products);
-        listView.setAdapter(myAdapter);
-        myAdapter.notifyDataSetChanged();
-        assertEquals(1, myAdapter.getCount());
         assertEquals(1, listView.getChildCount());
 
         View firstItem = listView.getChildAt(0);
         assertNotNull(firstItem);
+        
     }
-
 }
