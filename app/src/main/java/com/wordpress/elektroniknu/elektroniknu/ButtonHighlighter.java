c91743a15e6a98d906d353a094a58af4b39d4de0
vgrigorjeva <vgrigorjeva@gmail.com>
2015-04-29 10:15:23 +0200

    Improved feedback when touched
    Improved feedback when touched

--- app/src/main/java/com/wordpress/elektroniknu/elektroniknu/ButtonHighlighter.java
+++ app/src/main/java/com/wordpress/elektroniknu/elektroniknu/ButtonHighlighter.java
@@ -19,9 +19,9 @@ public class ButtonHighlighter implements View.OnTouchListener {
     public boolean onTouch(final View view, final MotionEvent motionEvent) {
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
             //grey color filter, you can change the color as you like
-            imageButton.setColorFilter(Color.argb(225, 185, 185, 185));
+            imageButton.setColorFilter(Color.argb(225, 246, 243, 243));
         } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-            imageButton.setColorFilter(Color.argb(0, 185, 185, 185));
+            imageButton.setColorFilter(Color.argb(0, 246, 243, 243));
         }
         return false;
     }
